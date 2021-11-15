b4d9460f551f452c66815cf76c297839886b84dd
Eugene Helland <eugenehelland@mail.com>
2021-11-15 16:08:20 -0700

    Cleaned up background image. Refactored some of the code into functions. Added functionality to the clear button. Cursor must now be dragged in order to draw.

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const playArea = document.querySelector('#play-area');
+const clearButton = document.querySelector('#clear');
 
-for (let i = 0; i < 5828; i++) {
-  let pixel = document.createElement('div');
-  pixel.classList.add('pixel');
+playArea.classList.add('no-pointers');
 
-  playArea.append(pixel);
+document.addEventListener('mousedown', () => {
+  playArea.classList.toggle('no-pointers');
+});
+
+document.addEventListener('mouseup', () => {
+  playArea.classList.add('no-pointers');
+});
+
+function populate() {
+  // Populate the pixels.
+  for (let i = 0; i < 5828; i++) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+
+    playArea.append(pixel);
+  }
+  // Add coloring functionality to new pixels.
+  let pixels = document.querySelectorAll('.pixel');
+
+  pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+      pixel.classList.add('filled');
+    });
+  });
 }
 
-let pixels = document.querySelectorAll('.pixel');
+function clearPlay() {
+  // Remove all pixels from play-area.
+  playArea.innerHTML = '';
+}
 
-pixels.forEach((pixel) => {
-  pixel.addEventListener('mouseover', () => {
-    pixel.classList.add('filled');
-  });
+populate();
+
+// Clear play-area and repopulate with fresh pixels.
+clearButton.addEventListener('click', () => {
+  clearPlay();
+  populate();
 });
