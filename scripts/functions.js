e1c3aedc678a01c23f2bb559122fe86d2db55039
Eugene Helland <eugenehelland@mail.com>
2021-11-16 09:55:11 -0700

    Pushing folder with js files. Had it stored in the wrong location.

---
+++ scripts/functions.js
@@ -0,0 +1,39 @@
+function populate(pixelDensity, pixelSize) {
+  // Populate the pixels.
+  for (let i = 0; i < pixelDensity; i++) {
+    let pixel = document.createElement('div');
+    // pixel.classList.add('pixel');
+    pixel.setAttribute(
+      'style',
+      `width: ${pixelSize}px; height: ${pixelSize}px;`
+    );
+
+    playArea.append(pixel);
+  }
+  // Add coloring functionality to new pixels.
+  // let pixels = document.querySelectorAll('.pixel');
+  let pixels = playArea.querySelectorAll(':scope > div');
+
+  pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+      pixel.classList.add('filled');
+    });
+  });
+}
+
+function clearPlay() {
+  // Remove all pixels from play-area.
+  playArea.innerHTML = '';
+}
+
+function changeRes(resolution) {
+  let height = resolution;
+  let width = height * 1.5;
+  let pixelDensity = height * width;
+
+  // Get pixel height and width.
+  let pixelSize = 500 / height;
+
+  clearPlay();
+  populate(pixelDensity, pixelSize);
+}
---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+const playArea = document.querySelector('#play-area');
+const lowRes = document.querySelector('#low');
+const medRes = document.querySelector('#medium');
+const highRes = document.querySelector('#high');
+const clearButton = document.querySelector('#clear');
+
+// Determines the resolution after the CLEAR button is pressed.
+let currentResolution = 32;
+
+// User must press and hold left mouse button to draw.
+playArea.classList.add('no-pointers');
+
+document.addEventListener('mousedown', () => {
+  playArea.classList.remove('no-pointers');
+});
+
+document.addEventListener('mouseup', () => {
+  playArea.classList.add('no-pointers');
+});
+
+playArea.addEventListener('mouseover mousedown', () => {
+  return false;
+});
+
+// Clear play-area and repopulate with fresh pixels.
+clearButton.addEventListener('click', () => {
+  clearPlay();
+  changeRes(currentResolution);
+});
+
+// Clear play-area and change resolutions.
+lowRes.addEventListener('click', () => {
+  currentResolution = 16;
+  changeRes(16);
+});
+
+medRes.addEventListener('click', () => {
+  currentResolution = 32;
+  changeRes(32);
+});
+
+highRes.addEventListener('click', () => {
+  currentResolution = 64;
+  changeRes(64);
+});
+
+// Populate starting pixels at medium resolution.
+changeRes(currentResolution);
