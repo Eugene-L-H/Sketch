132565d3ce0a701f3ba20c3dc22e08689b10d2c8
Eugene Helland <eugenehelland@mail.com>
2021-11-15 19:31:35 -0700

    Modified script.js file to accept dynamic values for the amount of pixels populated in the play area in preperation for user input.

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 const playArea = document.querySelector('#play-area');
 const clearButton = document.querySelector('#clear');
 
+let height = 45;
+let width = height * 1.5;
+let num = height * width;
+
+// Get pixel height and width.
+let pixelSize = 500 / height;
+
 playArea.classList.add('no-pointers');
 
 document.addEventListener('mousedown', () => {
-  playArea.classList.toggle('no-pointers');
+  playArea.classList.remove('no-pointers');
 });
 
 document.addEventListener('mouseup', () => {
   playArea.classList.add('no-pointers');
 });
 
-function populate() {
+function populate(num, pixelSize) {
   // Populate the pixels.
-  for (let i = 0; i < 5828; i++) {
+  for (let i = 0; i < num; i++) {
     let pixel = document.createElement('div');
-    pixel.classList.add('pixel');
+    // pixel.classList.add('pixel');
+    pixel.setAttribute(
+      'style',
+      `width: ${pixelSize}px; height: ${pixelSize}px;`
+    );
 
     playArea.append(pixel);
   }
   // Add coloring functionality to new pixels.
-  let pixels = document.querySelectorAll('.pixel');
+  // let pixels = document.querySelectorAll('.pixel');
+  let pixels = playArea.querySelectorAll(':scope > div');
 
   pixels.forEach((pixel) => {
     pixel.addEventListener('mouseover', () => {
@@ -34,10 +46,10 @@ function clearPlay() {
   playArea.innerHTML = '';
 }
 
-populate();
+populate(num, pixelSize);
 
 // Clear play-area and repopulate with fresh pixels.
 clearButton.addEventListener('click', () => {
   clearPlay();
-  populate();
+  populate(num, pixelSize);
 });
